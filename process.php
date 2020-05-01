42569b5ad8252c8b9f090a6ff7c6f7a2ae2cdcdf
calthompson <54155266+calthompson@users.noreply.github.com>
2020-05-01 11:19:26 -0400

    Added comments in process.php and in project.html

--- process.php
+++ process.php
@@ -1,5 +1,5 @@
 <?php
-
+// initializing the variables
 $searchQuery = '';
 $searchChoice = '';
 $xmlfile = simplexml_load_file("xml_data/response_full.xml");
@@ -10,6 +10,8 @@ $xmlfile = simplexml_load_file("xml_data/response_full.xml");
     $searchQuery = test_input($_POST['query']); // This will have the user's search query
     $searchQuery = ucwords(strtolower($searchQuery));
     $searchChoice = test_input($_POST['searchChoice']); // This will store the user's choice for what to search by
+
+// Execute different functions based on radio button choice
     if ($searchChoice == "name") {
       searchName($searchQuery,$xmlfile);
     } elseif ($searchChoice == "price") {
@@ -21,6 +23,7 @@ $xmlfile = simplexml_load_file("xml_data/response_full.xml");
     }
 }
 
+// Function to help sanitize data
 function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
@@ -29,120 +32,137 @@ function test_input($data) {
 }
 
 
-
+// Function to help sort the array alphabetically
 function compareName($nameA, $nameB) {
   $retrieval = strnatcmp($nameA->name,$nameB->name);
   return $retrieval;
 }
 
+// Search by Cuisine
 function searchStyle($search, $filename) {
+  // xpath search for categories with the search query, and put results into an array
   $path = "//businesses[categories/title[contains(text(),'".$search."')]]";
   $sortarray = array();
   foreach ($filename->xpath($path) as $businesses) {
     $sortarray[] = $businesses;
   }
+  // if a match is found, output an html table
   if (count($sortarray) != 0) {
     echo "<thead><tr><th>Business Name</th>".
     "<th>Business Address</th><th>Cuisine</th><th>Rating</th><th>Price</th></tr></thead>";
-  usort($sortarray, __NAMESPACE__ . '\compareName');
-  foreach ($sortarray as $businessinfo) {
-    $url = $businessinfo->url;
-    echo "<tr><td><a href='{$url}' target='_blank'>".$businessinfo->name."</a></td>".
-    "<td>".$businessinfo->location->address1."<br/>".$businessinfo->location->city.", OH</td>".
-    "<td>".$businessinfo->categories->title."</td>".
-    "<td>".$businessinfo->rating."</td>".
-    "<td>".$businessinfo->price."</td>".
-    "</tr>";
-  }} else {
-    echo "<h3>No results found</h3>";
-    return;
-  }
+    usort($sortarray, __NAMESPACE__ . '\compareName'); // initial alphabetical sort
+
+    foreach ($sortarray as $businessinfo) {
+      $url = $businessinfo->url;
+      echo "<tr><td><a href='{$url}' target='_blank'>".$businessinfo->name."</a></td>".
+      "<td>".$businessinfo->location->address1."<br/>".$businessinfo->location->city.", OH</td>".
+      "<td>".$businessinfo->categories->title."</td>".
+      "<td>".$businessinfo->rating."</td>".
+      "<td>".$businessinfo->price."</td>".
+      "</tr>";
+    }
+  } else {  // if nothing is found, let the user know there were no results
+      echo "<h3>No results found</h3>";
+      return;
+    }
   var_dump($sortarray);
 }
 
-
+// Search by Restaurant Name
 function searchName($search,$filename) {
+  // xpath search for categories with the search query, and put results into an array
   $path = "//businesses[name[contains(text(),'".$search."')]]";
   $sortarray = array();
   foreach ($filename->xpath($path) as $businesses) {
     $sortarray[] = $businesses;
   }
+  // if a match is found, output an html table
   if (count($sortarray) != 0) {
     echo "<thead><tr><th>Business Name</th>".
     "<th>Business Address</th><th>Cuisine</th><th>Rating</th><th>Price</th></tr></thead>";
-  usort($sortarray, __NAMESPACE__ . '\compareName');
-  foreach ($sortarray as $businessinfo) {
-    $url = $businessinfo->url;
-    echo "<tr><td><a href='{$url}' target='_blank'>".$businessinfo->name."</a></td>".
-    "<td>".$businessinfo->location->address1."<br/>".$businessinfo->location->city.", OH</td>".
-    "<td>".$businessinfo->categories->title."</td>".
-    "<td>".$businessinfo->rating."</td>".
-    "<td>".$businessinfo->price."</td>".
-    "</tr>";
-  }} else {
-    echo "<h3>No results found</h3>";
-    return;
-  }
+    usort($sortarray, __NAMESPACE__ . '\compareName'); // initial alphabetical sort
+
+    foreach ($sortarray as $businessinfo) {
+      $url = $businessinfo->url;
+      echo "<tr><td><a href='{$url}' target='_blank'>".$businessinfo->name."</a></td>".
+      "<td>".$businessinfo->location->address1."<br/>".$businessinfo->location->city.", OH</td>".
+      "<td>".$businessinfo->categories->title."</td>".
+      "<td>".$businessinfo->rating."</td>".
+      "<td>".$businessinfo->price."</td>".
+      "</tr>";
+    }
+  } else {  // if nothing is found, let the user know there were no results
+      echo "<h3>No results found</h3>";
+      return;
+    }
   var_dump($sortarray);
 }
-
+// Search by Restaurant Price
 function searchPrice($search,$filename) {
   if ($search == "") {
     $search = "$";
   }
+  // xpath search for categories with the search query, and put results into an array
   $path = "//businesses[price[text()='".$search."']]";
   $sortarray = array();
   foreach ($filename->xpath($path) as $businesses) {
     $sortarray[] = $businesses;
   }
+  // if any matches are found, output an html table
   if (count($sortarray) != 0) {
+    echo "<thead><tr><th>Business Name</th>".
+    "<th>Business Address</th><th>Cuisine</th><th>Rating</th><th>Price</th></tr></thead>";
+    usort($sortarray, __NAMESPACE__ . '\compareName'); // initial alphabetical sort
 
-  echo "<thead><tr><th>Business Name</th>".
-  "<th>Business Address</th><th>Cuisine</th><th>Rating</th><th>Price</th></tr></thead>";
-    usort($sortarray, __NAMESPACE__ . '\compareName');
     foreach ($sortarray as $businessinfo) {
-    $url = $businessinfo->url;
-    echo "<tr><td><a href='{$url}' target='_blank'>".$businessinfo->name."</a></td>".
-    "<td>".$businessinfo->location->address1."<br/>".$businessinfo->location->city.", OH</td>".
-    "<td>".$businessinfo->categories->title."</td>".
-    "<td>".$businessinfo->rating."</td>".
-    "<td>".$businessinfo->price."</td>".
-    "</tr>";
-  }} else {
-    echo "<h3>No results found</h3>";
-    return;
-  }
+      $url = $businessinfo->url;
+      echo "<tr><td><a href='{$url}' target='_blank'>".$businessinfo->name."</a></td>".
+      "<td>".$businessinfo->location->address1."<br/>".$businessinfo->location->city.", OH</td>".
+      "<td>".$businessinfo->categories->title."</td>".
+      "<td>".$businessinfo->rating."</td>".
+      "<td>".$businessinfo->price."</td>".
+      "</tr>";
+    }
+  } else { // if nothing is found, let the user know there were no results
+      echo "<h3>No results found</h3>";
+      return;
+    }
   var_dump($sortarray);
 }
 
 
 
-
+// Search by Restaurant Rating (out of 5)
 function searchRating($search,$filename) {
   if ($search == "") {
     $search = 0;
   }
+  // xpath search for categories with the search query, and put results into an array
   $path = "//businesses[rating>=".$search."]";
   $sortarray = array();
   foreach ($filename->xpath($path) as $businesses) {
     $sortarray[] = $businesses;
   }
-    usort($sortarray, __NAMESPACE__ . '\compareName');
-if (count($sortarray) != 0) {
-  echo "<thead><tr><th>Business Name</th>".
-  "<th>Business Address</th><th>Cuisine</th><th>Rating</th><th>Price</th></tr></thead>";
+
+  // if any matches are found, output an html table
+  if (count($sortarray) != 0) {
+    echo "<thead><tr><th>Business Name</th>".
+    "<th>Business Address</th><th>Cuisine</th><th>Rating</th><th>Price</th></tr></thead>";
+    usort($sortarray, __NAMESPACE__ . '\compareName'); // initial alphabetical sort
+
     foreach ($sortarray as $businessinfo) {
-    $url = $businessinfo->url;
-    echo "<tbody><tr><td><a href='{$url}' target='_blank'>".$businessinfo->name."</a></td>".
-    "<td>".$businessinfo->location->address1."<br/>".$businessinfo->location->city.", OH</td>".
-    "<td>".$businessinfo->categories->title."</td>".
-    "<td>".$businessinfo->rating."</td>".
-    "<td>".$businessinfo->price."</td>".
-    "</tr></tbody>";
-  }} else {
-    echo "<h3>No results found</h3>";
-    return;
-  }
+      $url = $businessinfo->url;
+      echo "<tbody><tr><td><a href='{$url}' target='_blank'>".$businessinfo->name."</a></td>".
+      "<td>".$businessinfo->location->address1."<br/>".$businessinfo->location->city.", OH</td>".
+      "<td>".$businessinfo->categories->title."</td>".
+      "<td>".$businessinfo->rating."</td>".
+      "<td>".$businessinfo->price."</td>".
+      "</tr></tbody>";
+    }
+  } else {  // if nothing is found, let the user know there were no results
+      echo "<h3>No results found</h3>";
+      return;
+    }
   var_dump($sortarray);
 
 }
